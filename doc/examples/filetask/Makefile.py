9f349243e19dfb598058747bb10424a328de35a2
Trent Mick <trentm@activestate.com>
2010-08-23 10:31:13 -0700

    move output to 'tmp' dir for easier gitignore

--- doc/examples/filetask/Makefile.py
+++ doc/examples/filetask/Makefile.py
@@ -1,13 +1,19 @@
+from os.path import exists, dirname
+import os
 from mklib import Task
 
 class answer(Task):
     default = True
     deps = ["one.txt", "two.txt"]
-    results = ["answer.txt"]
+    results = ["tmp/answer.txt"]
     def make(self):
         answer = 0
         for dep in self.deps:
             answer += int(open(dep.path).read().strip())
         self.log.info("answer is %r" % answer)
-        open(self.results[0].path, 'w').write(str(answer))
+
+        result = self.results[0].path
+        if not exists(dirname(result)):
+            os.mkdir(dirname(result))
+        open(result, 'w').write(str(answer))
 
