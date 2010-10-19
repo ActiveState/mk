3d92212b59ecb41924a13c7cea2d66ebc2d5ac61
Trent Mick <trentm@gmail.com>
2010-10-19 16:29:27 -0700

    indentation, no functionl change

--- setup.py
+++ setup.py
@@ -8,28 +8,29 @@ sys.path.insert(0, path.abspath(path.dirname(__file__)))
 import mklib
 
 
-setup(name='mklib',
-      version=mklib.__version__,
-      description="a Makefile/make replacement written in Python (used at ActiveState)",
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-      ],
-      keywords='make mk',
-      author='Trent Mick',
-      author_email='trentm@gmail.com',
-      maintainer='Sridhar Ratnakumar',
-      maintainer_email='sridharr@activestate.com',
-      url='http://github.com/ActiveState/mk',
-      license='MIT',
-      packages=find_packages(exclude=[
-          'examples', 'test']),
-      include_package_data=True,
-      zip_safe=False,
-      entry_points={
-        'console_scripts': [
-            'mk=mklib.runner:main'
-        ]
-      },
+setup(
+    name='mklib',
+    version=mklib.__version__,
+    description="a Makefile/make replacement written in Python (used at ActiveState)",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='make mk',
+    author='Trent Mick',
+    author_email='trentm@gmail.com',
+    maintainer='Sridhar Ratnakumar',
+    maintainer_email='sridharr@activestate.com',
+    url='http://github.com/ActiveState/mk',
+    license='MIT',
+    packages=find_packages(exclude=[
+        'examples', 'test']),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+      'console_scripts': [
+          'mk=mklib.runner:main'
+      ]
+    },
 )
